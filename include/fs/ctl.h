8f090ab1879c954960450ea3ab133b85765e890b
gunnerDgd <ifstudiomail@naver.com>
2024-07-24 22:55:40 +0900

    [Feature] fs_ctl : added pp_cast for casting to pp_ua

--- include/fs/ctl.h
+++ include/fs/ctl.h
@@ -9,9 +9,6 @@ typedef struct       fs_ctl { u8_t ctl[128]; } fs_ctl;
 
 void    fs_ctl_ready  (fs_ctl*, i64_t);
 u64_t   fs_ctl_code   (fs_ctl*);
-
-any_t   fs_ctl_arg_ka (fs_ctl*, u64_t, pp_mem*);
-pp_ua*  fs_ctl_arg_ua (fs_ctl*, u64_t);
 any_t   fs_ctl_arg    (fs_ctl*);
 
 #endif
--- src/ctl.c
+++ src/ctl.c
@@ -3,13 +3,25 @@
 
 #include <linux/uaccess.h>
 
+static pp_obj*
+    do_as
+        (fs_ctl* self, pp_obj_trait* as)                    {
+            if (pp_trait_of(self) != fs_ctl_t) return null_t;
+            if (as != pp_ua_t)                 return null_t;
+            return pp_make (pp_ua) from (
+                2        ,
+                self->arg,
+                -1
+            );
+}
+
 static bool_t
     do_new
         (fs_ctl* self, u32_t count, va_list par)                            {
-            pp_wait *wait = null_t; if (count > 3) wait = va_arg(par, any_t);
             fs_file *file = null_t; if (count > 0) file = va_arg(par, any_t);
             u64_t    code = 0     ; if (count > 1) code = va_arg(par, u64_t);
             any_t    arg  = 0     ; if (count > 2) arg  = va_arg(par, any_t);
+            pp_wait *wait = null_t; if (count > 3) wait = va_arg(par, any_t);
             if (pp_trait_of(file) != fs_file_t) return false_t;
 
             self->wait = (pp_wait*) pp_ref(wait);
@@ -57,44 +69,9 @@ void
 any_t fs_ctl_arg  (fs_ctl* self) { if (pp_trait_of(self) != fs_ctl_t) return null_t; return self->arg ; }
 u64_t fs_ctl_code (fs_ctl* self) { if (pp_trait_of(self) != fs_ctl_t) return -1;     return self->code; }
 
-any_t
-    fs_ctl_arg_ka
-        (fs_ctl* self, u64_t len, pp_mem* mem)                   {
-            if (pp_trait_of(mem)  != pp_mem_t) mem = pp_get_mem();
-            if (pp_trait_of(mem)  != pp_mem_t) return null_t;
-            if (pp_trait_of(self) != fs_ctl_t) return null_t;
-            if (!len)                          return null_t;
-            any_t ret = pp_mem_use(mem, null_t, len);
-
-            if (!ret) return null_t;
-            copy_from_user         (
-                ret      ,
-                self->arg,
-                len
-            );
-
-            return ret;
-
-}
-
-pp_ua*
-    fs_ctl_arg_ua
-        (fs_ctl* self, u64_t len)                           {
-            if (pp_trait_of(self) != fs_ctl_t) return null_t;
-            if (!len)                          return null_t;
-            return pp_make (pp_ua) from (
-                2        ,
-                self->arg,
-                len
-            );
-}
-
 #include <linux/module.h>
 EXPORT_SYMBOL(fs_ctl_ready);
 EXPORT_SYMBOL(fs_ctl_code);
-
-EXPORT_SYMBOL(fs_ctl_arg_ka);
-EXPORT_SYMBOL(fs_ctl_arg_ua);
 EXPORT_SYMBOL(fs_ctl_arg);
 
 EXPORT_SYMBOL(fs_ctl_t);
